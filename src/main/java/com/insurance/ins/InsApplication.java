e6b797f5ac60f14b9057b73c3f7bb58e3e2a7304
Krasimir Gyorev <krasi_job@mail.bg>
2018-03-21 22:36:12 +0200

    Init

---
+++ src/main/java/com/insurance/ins/InsApplication.java
@@ -0,0 +1,12 @@
+package com.insurance.ins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InsApplication.class, args);
+	}
+}
